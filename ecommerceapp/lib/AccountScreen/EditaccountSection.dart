b12b660b3d2c659d74a9084ee5e571c9a55b052f
Hassan Ali <hassanali22857@gmail.com>
2024-05-14 17:55:13 +0500

    change Image Function done

--- ecommerceapp/lib/AccountScreen/EditaccountSection.dart
+++ ecommerceapp/lib/AccountScreen/EditaccountSection.dart
@@ -1,54 +1,71 @@
+import 'dart:io';
+
 import 'package:ecommerceapp/CommonWidgets/Buttons.dart';
 import 'package:ecommerceapp/CommonWidgets/CustomTextFields.dart';
 import 'package:ecommerceapp/CommonWidgets/common_bg.dart';
+import 'package:ecommerceapp/HomeController/Account_Controller.dart';
 import 'package:ecommerceapp/conts/consts.dart';
+import 'package:get/get.dart';
 
 class EditScreen extends StatelessWidget {
   const EditScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var controller = Get.put(profileController());
+    Get.find<profileController>();
     return commonBackground(
         child: Scaffold(
       appBar: AppBar(),
       body: SingleChildScrollView(
         physics: BouncingScrollPhysics(),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              imgProfile2,
-              width: 90,
-              fit: BoxFit.cover,
-            ).box.roundedFull.clip(Clip.antiAlias).make(),
-            10.heightBox,
-            ourButtons(
-                title: 'ChangeImage',
-                color: redColor,
-                onPress: () {},
-                textColor: whiteColor),
-            20.heightBox,
-            customTextField(hint: namehint, Ispass: false, title: name),
-            10.heightBox,
-            customTextField(hint: PasswordHint, Ispass: true, title: password),
-            30.heightBox,
-            SizedBox(
-              width: context.screenWidth - 70,
-              child: ourButtons(
-                  title: 'Save',
+        child: Obx(
+          () => Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              controller.profileimagepath.isEmpty
+                  ? Image.asset(
+                      imgProfile2,
+                      width: 110,
+                      fit: BoxFit.cover,
+                    ).box.roundedFull.clip(Clip.antiAlias).make()
+                  : Image.file(
+                      File(controller.profileimagepath.value),
+                      width: 110,
+                      fit: BoxFit.cover,
+                    ).box.roundedFull.clip(Clip.antiAlias).make(),
+              10.heightBox,
+              ourButtons(
+                  title: 'ChangeImage',
                   color: redColor,
-                  onPress: () {},
+                  onPress: () {
+                    controller.changeimagepath(context);
+                  },
                   textColor: whiteColor),
-            ),
-          ],
-        )
-            .box
-            .white
-            .padding(EdgeInsets.all(12))
-            .rounded
-            .shadowSm
-            .margin(EdgeInsets.all(15))
-            .make(),
+              20.heightBox,
+              customTextField(hint: namehint, Ispass: false, title: name),
+              10.heightBox,
+              customTextField(
+                  hint: PasswordHint, Ispass: true, title: password),
+              30.heightBox,
+              SizedBox(
+                width: context.screenWidth - 70,
+                child: ourButtons(
+                    title: 'Save',
+                    color: redColor,
+                    onPress: () {},
+                    textColor: whiteColor),
+              ),
+            ],
+          )
+              .box
+              .white
+              .padding(EdgeInsets.all(12))
+              .rounded
+              .shadowSm
+              .margin(EdgeInsets.all(15))
+              .make(),
+        ),
       ),
     ));
   }
--- ecommerceapp/lib/HomeController/Account_Controller.dart
+++ ecommerceapp/lib/HomeController/Account_Controller.dart
@@ -1,12 +1,21 @@
+import 'package:ecommerceapp/conts/consts.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 class profileController extends GetxController {
-
   var profileimagepath = ''.obs;
 
-  changeimagepath(){
-
-    
-
+  changeimagepath(context) async {
+    try {
+      final img = await ImagePicker()
+          .pickImage(source: ImageSource.gallery, imageQuality: 70);
+      if (img == null) {
+        return;
+      }
+      profileimagepath.value = img.path;
+    } on PlatformException catch (e) {
+      VxToast.show(context, msg: e.toString());
+    }
   }
 }
